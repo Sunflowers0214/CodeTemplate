9cc0bb471077d15f5ec656bcc113ff91bacee23c
黄永生 <972855727@qq.com>
2019-03-01 14:04:31 +0800

    修正

--- src/main/java/com/codgen/main/CreateCode.java
+++ src/main/java/com/codgen/main/CreateCode.java
@@ -1,11 +1,13 @@
 package com.codgen.main;
 
 import com.codgen.helper.DataHelper;
+import com.codgen.model.Convert;
 import com.codgen.model.JdbcConfig;
-import com.codgen.model.TableConfig;
 import com.codgen.model.TableModel;
 import com.codgen.helper.BuilderHelper;
 import com.codgen.helper.FileHelper;
+import com.codgen.model.VelocityConfig;
+import org.apache.commons.lang.StringUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
@@ -20,8 +22,6 @@ public class CreateCode {
     private static List<TableModel> tableModelList;
     private static VelocityContext velocityContext;
     private static FileHelper helper = new FileHelper();
-    private static String templatePath;
-    private static String projectName;
 
     public static void main(String[] args) throws IOException {
         Properties properties = helper.getProperties("jdbc.properties");
@@ -32,94 +32,51 @@ public class CreateCode {
         velocityContext = CreateCode.initVelocityContext(properties);
 
         //模板路径
-        templatePath = properties.getProperty("templetFilePath");
+        String templatePath = properties.getProperty("templetFilePath");
         //项目名
-        projectName = properties.get("projectName").toString();
+        String projectName = properties.get("projectName").toString();
         //文件生成路径
         String createFilePath = properties.getProperty("genDirPath");
         String javaPath = createFilePath + File.separator + projectName + "/java/" + projectName;
         String testPath = createFilePath + File.separator + projectName + "/test/" + projectName;
         String configPath = createFilePath + File.separator + projectName + "/config";
-
+        String webPath = createFilePath + File.separator + projectName + "/web/" + projectName;
+        List<VelocityConfig> velocityList = new ArrayList<>();
         //实体类
-        createModel(javaPath);
+        velocityList.add(new VelocityConfig(templatePath + "Model.vm", javaPath + "/model", ".java"));
         //控制层
-        createController(javaPath);
+        velocityList.add(new VelocityConfig(templatePath + "Controller.vm", javaPath + "/controller", "Controller.java"));
+        velocityList.add(new VelocityConfig(templatePath + "ControllerTest.vm", testPath + "/controller", "ControllerTest.java"));
         //逻辑处理层
-        createService(javaPath);
+        velocityList.add(new VelocityConfig(templatePath + "Service.vm", javaPath + "/service", "Service.java"));
+        velocityList.add(new VelocityConfig(templatePath + "ServiceImpl.vm", javaPath + "/service/impl", "ServiceImpl.java"));
+        velocityList.add(new VelocityConfig(templatePath + "Service-config.vm", configPath + "/spring/", projectName + "-service.xml", true));
+        velocityList.add(new VelocityConfig(templatePath + "ServiceTest.vm", testPath + "/service", "ServiceTest.java"));
         //数据库访问层
-        createDao(javaPath);
+        velocityList.add(new VelocityConfig(templatePath + "Dao.vm", javaPath + "/dao", "Dao.java"));
+        velocityList.add(new VelocityConfig(templatePath + "DaoImpl.vm", javaPath + "/dao/impl", "DaoImpl.java"));
+        velocityList.add(new VelocityConfig(templatePath + "Dao-config.vm", configPath + "/spring/", projectName + "-dao.xml", true));
+        velocityList.add(new VelocityConfig(templatePath + "DaoTest.vm", testPath + "/dao", "DaoTest.java"));
         //mybatis
-        createMybatis(configPath);
-        //SpringXml
-        createSpringXml(configPath);
-        //Test
-        createTest(testPath);
+        velocityList.add(new VelocityConfig(templatePath + "mybatis.vm", configPath + "/mybatis/" + projectName, "Mapper.java"));
+        velocityList.add(new VelocityConfig(templatePath + "mybatis-config.vm", configPath + "/mybatis", "mybatis.config." + projectName + ".xml", true));
+        velocityList.add(new VelocityConfig(templatePath + "mybatis-spring.vm", configPath + "/spring", "spring-mybatis-" + projectName + "-config.xml", true));
 
-        String webPath = createFilePath + "web/" + projectName;
         //HTML
-        createHtml(webPath);
-        //Language
-        createLanguage(webPath);
+        velocityList.add(new VelocityConfig(templatePath + "web-html-man.vm", webPath + "/html", "Man.html"));
+        velocityList.add(new VelocityConfig(templatePath + "web-html-add.vm", webPath + "/html", "Add.html"));
+        velocityList.add(new VelocityConfig(templatePath + "web-html-modify.vm", webPath + "/html", "Modify.html"));
+        velocityList.add(new VelocityConfig(templatePath + "web-html-detail.vm", webPath + "/html", "Detail.html"));
         //JS
-        //createJs(webPath);
-    }
+        velocityList.add(new VelocityConfig(templatePath + "web-js-man.vm", webPath + "/js", "Man.js"));
+        velocityList.add(new VelocityConfig(templatePath + "web-js-add.vm", webPath + "/js", "Add.js"));
+        velocityList.add(new VelocityConfig(templatePath + "web-js-modify.vm", webPath + "/js", "Modify.js"));
+        velocityList.add(new VelocityConfig(templatePath + "web-js-detail.vm", webPath + "/js", "Detail.js"));
+        //Language
+        velocityList.add(new VelocityConfig(templatePath + "web-language-en.vm", webPath + "/language", "Page.js"));
+        velocityList.add(new VelocityConfig(templatePath + "web-language-zn.vm", webPath + "/language", "Page.js"));
 
-    private static void createModel(String javaPath) throws IOException {
-        CreateCode.createFiles(templatePath + "Model.vm", javaPath + "/model", ".java");
-    }
-
-    private static void createController(String javaPath) throws IOException {
-        CreateCode.createFiles(templatePath + "Controller.vm", javaPath + "/controller", "Controller.java");
-    }
-
-    private static void createService(String javaPath) throws IOException {
-        CreateCode.createFiles(templatePath + "Service.vm", javaPath + "/service", "Service.java");
-        CreateCode.createFiles(templatePath + "ServiceImpl.vm", javaPath + "/service/impl", "ServiceImpl.java");
-    }
-
-    private static void createDao(String javaPath) throws IOException {
-        CreateCode.createFiles(templatePath + "Dao.vm", javaPath + "/dao", "Dao.java");
-        CreateCode.createFiles(templatePath + "DaoImpl.vm", javaPath + "/dao/impl", "DaoImpl.java");
-    }
-
-    private static void createSpringXml(String configPath) throws IOException {
-        CreateCode.createFile(templatePath + "Dao-config.vm", configPath + "/spring", projectName + "-dao.xml");
-        CreateCode.createFile(templatePath + "Service-config.vm", configPath + "/spring", projectName + "-service.xml");
-    }
-
-    private static void createTest(String testPath) throws IOException {
-        CreateCode.createFiles(templatePath + "DaoTest.vm", testPath + "/dao", "DaoTest.java");
-        CreateCode.createFiles(templatePath + "ServiceTest.vm", testPath + "/service", "ServiceTest.java");
-        CreateCode.createFiles(templatePath + "ControllerTest.vm", testPath + "/controller", "ControllerTest.java");
-    }
-
-    private static void createMybatis(String configPath) throws IOException {
-        CreateCode.createFiles(templatePath + "mybatis.vm", configPath + "/mybatis/" + projectName, "Mapper.xml");
-        CreateCode.createFile(templatePath + "mybatis-config.vm", configPath + "/mybatis", "mybatis.config." + projectName + ".xml");
-        CreateCode.createFile(templatePath + "mybatis-spring.vm", configPath + "/spring", "spring-mybatis-" + projectName + "-config.xml");
-    }
-
-    private static void createHtml(String webPath) throws IOException {
-        String path = webPath + "/html";
-        CreateCode.createFiles(templatePath + "web-manhtml.vm", path, "Man.html");
-        CreateCode.createFiles(templatePath + "web-addhtml.vm", path, "Add.html");
-        CreateCode.createFiles(templatePath + "web-modifyhtml.vm", path, "Modify.html");
-        CreateCode.createFiles(templatePath + "web-detailhtml.vm", path, "Detail.html");
-    }
-
-    private static void createJs(String webPath) throws IOException {
-        String path = webPath + "/js";
-        CreateCode.createFiles(templatePath + "web-manjs.vm", path, "Man,js");
-        CreateCode.createFiles(templatePath + "web-addjs.vm", path, "Add.js");
-        CreateCode.createFiles(templatePath + "web-modifyjs.vm", path, "Modify.js");
-        CreateCode.createFiles(templatePath + "web-detailjs.vm", path, "Detail.js");
-    }
-
-    private static void createLanguage(String webPath) throws IOException {
-        String path = webPath + "/language";
-        CreateCode.createFiles(templatePath + "web-languageEn.vm", path, "Page.js");
-        CreateCode.createFiles(templatePath + "web-languageZn.vm", path, "Page.js");
+        createFile(velocityList);
     }
 
     /**
@@ -129,12 +86,42 @@ public class CreateCode {
      * @return
      */
     private static List<TableModel> initTableModel(Properties properties) {
-        List<TableModel> tableList = new ArrayList<>();
         JdbcConfig jdbcConfig = new JdbcConfig(properties);
-        TableConfig tableConfig = new TableConfig(properties);
         DataHelper DataHelper = new DataHelper(jdbcConfig);
-        tableList = DataHelper.initTableModelList(tableConfig);
+        List<TableModel> tableList = DataHelper.initTableModelList();
+        for (TableModel tableModel : tableList) {
+            handleTableConvert(tableModel);
+        }
         return tableList;
+    }
+
+    /**
+     * 定义生成文件名称
+     *
+     * @param table
+     */
+    public static void handleTableConvert(TableModel table) {
+        Convert convert = new Convert();
+        convert.setModel(BuilderHelper.firstToUpper(table.getTableLabel()) + ".java");
+        convert.setController(BuilderHelper.firstToUpper(table.getTableLabel()) + "Controller.java");
+        convert.setControllerTest(BuilderHelper.firstToUpper(table.getTableLabel()) + "ControllerTest.java");
+        convert.setService(BuilderHelper.firstToUpper(table.getTableLabel()) + "Service.java");
+        convert.setServiceImpl(BuilderHelper.firstToUpper(table.getTableLabel()) + "ServiceImpl.java");
+        convert.setServiceTest(BuilderHelper.firstToUpper(table.getTableLabel()) + "ServiceTest.java");
+        convert.setDao(BuilderHelper.firstToUpper(table.getTableLabel()) + "Dao.java");
+        convert.setDaoImpl(BuilderHelper.firstToUpper(table.getTableLabel()) + "DaoImpl.java");
+        convert.setDaoTest(BuilderHelper.firstToUpper(table.getTableLabel()) + "DaoTest.java");
+        convert.setMybatis(BuilderHelper.firstToUpper(table.getTableLabel()) + "Mapper.xml");
+        convert.setHtmlMan(BuilderHelper.firstToUpper(table.getTableLabel()) + "Man.html");
+        convert.setHtmlAdd(BuilderHelper.firstToUpper(table.getTableLabel()) + "Add.html");
+        convert.setHtmlModify(BuilderHelper.firstToUpper(table.getTableLabel()) + "Modify.html");
+        convert.setHtmlDetail(BuilderHelper.firstToUpper(table.getTableLabel()) + "Detail.html");
+        convert.setJsMan(BuilderHelper.firstToUpper(table.getTableLabel()) + "Man.js");
+        convert.setJsAdd(BuilderHelper.firstToUpper(table.getTableLabel()) + "Add.js");
+        convert.setJsModify(BuilderHelper.firstToUpper(table.getTableLabel()) + "Modify.js");
+        convert.setJsDetail(BuilderHelper.firstToUpper(table.getTableLabel()) + "Detail.js");
+        convert.setLanguage(BuilderHelper.firstToUpper(table.getTableLabel()) + "Page.js");
+        table.setConvert(convert);
     }
 
     /**
@@ -167,44 +154,42 @@ public class CreateCode {
     }
 
     /**
-     * 生成多个文件,每个表对应一个文件
+     * 生成文件,对应一个文件
      *
-     * @param templateName 模板文件名
-     * @param dirName      目录
-     * @param suffix       文件名后缀
+     * @param velocityList
      */
-    private static void createFiles(String templateName, String dirName, String suffix) throws IOException {
-        FileHelper helper = new FileHelper();
-
+    private static void createFile(List<VelocityConfig> velocityList) throws IOException {
         // 初始化一个模板引擎
         Velocity.init();
         // 实例化一个VelocityEngine对象
         Template template = null;
-        for (TableModel tableModel : tableModelList) {
-            StringWriter sw = new StringWriter();
-            velocityContext.put("tableModel", tableModel);
-            // 将需要的数据加载到模板引擎的上下文中
-            // 从vm目录下加载hello.vm模板,在eclipse工程中该vm目录与src目录平级
+        for (VelocityConfig velocity : velocityList) {
+            String templateName = velocity.getTemplateName();
             template = Velocity.getTemplate(templateName, "UTF-8");
-            template.merge(velocityContext, sw);
-            String fileName = tableModel.getTableLabel();
-            fileName = BuilderHelper.firstToUpper(fileName);
-            helper.writeToFile(dirName + File.separator, fileName + suffix, sw.toString());
+            String filePath = velocity.getFilePath() + File.separator;
+            if (velocity.isConfigFlag()) {
+                String fileName = velocity.getSuffix();
+                StringWriter sw = new StringWriter();
+                template.merge(velocityContext, sw);
+                helper.writeToFile(filePath, fileName, sw.toString());
+                System.out.println("生成文件：" + filePath + fileName);
+            } else {
+                for (TableModel tableModel : tableModelList) {
+                    velocityContext.put("tableModel", tableModel);
+                    StringWriter sw = new StringWriter();
+                    template.merge(velocityContext, sw);
+                    String fileName = BuilderHelper.firstToUpper(tableModel.getTableLabel());
+                    if (StringUtils.isNotEmpty(velocity.getPrefix())) {
+                        fileName = velocity.getPrefix() + fileName;
+                    }
+                    if (StringUtils.isNotEmpty(velocity.getSuffix())) {
+                        fileName = fileName + velocity.getSuffix();
+                    }
+                    helper.writeToFile(filePath, fileName, sw.toString());
+                    System.out.println("生成文件：" + filePath + fileName);
+                }
+            }
         }
-    }
-
-    /**
-     * 生成文件,对应一个文件
-     *
-     * @param templateName
-     * @param dirName      文件路径
-     * @param fileName
-     */
-    private static void createFile(String templateName, String dirName, String fileName) throws IOException {
-        Template template = Velocity.getTemplate(templateName, "UTF-8");
-        StringWriter sw = new StringWriter();
-        template.merge(velocityContext, sw);
-        helper.writeToFile(dirName + File.separator, fileName, sw.toString());
     }
 
 }
---
+++ src/main/java/com/codgen/model/Convert.java
@@ -0,0 +1,178 @@
+package com.codgen.model;
+
+/**
+ * 表模型
+ */
+public class Convert {
+    private String model;
+    private String controller;
+    private String controllerTest;
+    private String service;
+    private String serviceImpl;
+    private String serviceTest;
+    private String dao;
+    private String daoImpl;
+    private String daoTest;
+    private String mybatis;
+    private String htmlMan;
+    private String htmlAdd;
+    private String htmlModify;
+    private String htmlDetail;
+    private String jsMan;
+    private String jsAdd;
+    private String jsModify;
+    private String jsDetail;
+    private String language;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getController() {
+        return controller;
+    }
+
+    public void setController(String controller) {
+        this.controller = controller;
+    }
+
+    public String getControllerTest() {
+        return controllerTest;
+    }
+
+    public void setControllerTest(String controllerTest) {
+        this.controllerTest = controllerTest;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getServiceImpl() {
+        return serviceImpl;
+    }
+
+    public void setServiceImpl(String serviceImpl) {
+        this.serviceImpl = serviceImpl;
+    }
+
+    public String getServiceTest() {
+        return serviceTest;
+    }
+
+    public void setServiceTest(String serviceTest) {
+        this.serviceTest = serviceTest;
+    }
+
+    public String getDao() {
+        return dao;
+    }
+
+    public void setDao(String dao) {
+        this.dao = dao;
+    }
+
+    public String getDaoImpl() {
+        return daoImpl;
+    }
+
+    public void setDaoImpl(String daoImpl) {
+        this.daoImpl = daoImpl;
+    }
+
+    public String getDaoTest() {
+        return daoTest;
+    }
+
+    public void setDaoTest(String daoTest) {
+        this.daoTest = daoTest;
+    }
+
+    public String getMybatis() {
+        return mybatis;
+    }
+
+    public void setMybatis(String mybatis) {
+        this.mybatis = mybatis;
+    }
+
+    public String getHtmlMan() {
+        return htmlMan;
+    }
+
+    public void setHtmlMan(String htmlMan) {
+        this.htmlMan = htmlMan;
+    }
+
+    public String getHtmlAdd() {
+        return htmlAdd;
+    }
+
+    public void setHtmlAdd(String htmlAdd) {
+        this.htmlAdd = htmlAdd;
+    }
+
+    public String getHtmlModify() {
+        return htmlModify;
+    }
+
+    public void setHtmlModify(String htmlModify) {
+        this.htmlModify = htmlModify;
+    }
+
+    public String getHtmlDetail() {
+        return htmlDetail;
+    }
+
+    public void setHtmlDetail(String htmlDetail) {
+        this.htmlDetail = htmlDetail;
+    }
+
+    public String getJsMan() {
+        return jsMan;
+    }
+
+    public void setJsMan(String jsMan) {
+        this.jsMan = jsMan;
+    }
+
+    public String getJsAdd() {
+        return jsAdd;
+    }
+
+    public void setJsAdd(String jsAdd) {
+        this.jsAdd = jsAdd;
+    }
+
+    public String getJsModify() {
+        return jsModify;
+    }
+
+    public void setJsModify(String jsModify) {
+        this.jsModify = jsModify;
+    }
+
+    public String getJsDetail() {
+        return jsDetail;
+    }
+
+    public void setJsDetail(String jsDetail) {
+        this.jsDetail = jsDetail;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
