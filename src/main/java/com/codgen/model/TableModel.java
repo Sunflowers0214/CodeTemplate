7decc104e62bcad7ca39c3245020ecf01c44fb1e
黄永生 <972855727@qq.com>
2019-03-01 13:59:31 +0800

    修正

--- src/main/java/com/codgen/model/TableModel.java
+++ src/main/java/com/codgen/model/TableModel.java
@@ -2,7 +2,6 @@ package com.codgen.model;
 
 import java.util.List;
 
-
 /**
  * 表模型
  */
@@ -10,12 +9,9 @@ public class TableModel {
     private String tableName;//表名
     private String tableLabel;//表标签
     private String tableComment = "";//表注释(中文)
-    private List<ColumnModel> columnList;//列模型集合
-    private List<ColumnModel> columnPKList;//主键列模型集合
-    private DeleteModel deleteModel;//逻辑删除模型
+    private Convert convert; //
 
-    private TableModel() {
-    }
+    private List<ColumnModel> columnList;//列模型集合
 
     public TableModel(String tableName, String tableComment) {
         this.tableName = tableName;
@@ -46,6 +42,14 @@ public class TableModel {
         this.tableComment = tableComment;
     }
 
+    public Convert getConvert() {
+        return convert;
+    }
+
+    public void setConvert(Convert convert) {
+        this.convert = convert;
+    }
+
     public List<ColumnModel> getColumnList() {
         return columnList;
     }
@@ -53,39 +57,5 @@ public class TableModel {
     public void setColumnList(List<ColumnModel> columnList) {
         this.columnList = columnList;
     }
-
-    public List<ColumnModel> getColumnPKList() {
-        return columnPKList;
-    }
-
-    public void setColumnPKList(List<ColumnModel> columnPKList) {
-        this.columnPKList = columnPKList;
-    }
-
-    public DeleteModel getDeleteModel() {
-        return deleteModel;
-    }
-
-    public void setDeleteModel(DeleteModel deleteModel) {
-        this.deleteModel = deleteModel;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("<" + tableComment + ">模型：{");
-        builder.append("tableName=" + tableName);
-        builder.append(", tableLabel=" + tableLabel);
-        builder.append(", tableComment=" + tableComment);
-        builder.append(", \n>>>表字段集合columnList=");
-        builder.append(columnList);
-        builder.append(", \n>>>主键列集合columnPKList=");
-        builder.append(columnPKList);
-        builder.append(", \n>>>逻辑删除deleteModel=");
-        builder.append(deleteModel);
-        builder.append("}\n");
-        return builder.toString();
-    }
-
 
 }
